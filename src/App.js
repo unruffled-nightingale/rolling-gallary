a8ac01f2ff4250c2cae9bc323d094fc7d8308a74
robert manteghi <unruffled.nightingale@gmail.com>
2022-03-29 23:04:48 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react'
+import Ball from './Ball.js';
+import Box from './Box.js';
+import Plane from './Plane.js';
+import Hud from './Hud.js';
+import Spotlight from './Spotlight.js';
+import { useState } from "react"
+import { Canvas } from '@react-three/fiber'
+import { RecoilRoot } from "recoil";
+import { Physics } from '@react-three/cannon'
+
+import './App.css'
 
 function App() {
+
+  const [hudKey, setHudKey] = useState(undefined);
+
+  const data = {
+    "face": { filename: "face.svg", price: 50, title: "face" }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Hud data={data[hudKey]} setHudKey={setHudKey}/>
+      <Canvas shadows camera={{ fov: 75, near: 0.1, far: 1000, position: [-20, 10, 0] }}>
+        <color attach="background" args={["black"]}/>
+        <Physics
+          gravity={[0, -9.81, 0]}
+          defaultContactMaterial={{
+            friction: 9,
+            restitution: 0.7,
+            contactEquationStiffness: 1e9,
+            contactEquationRelaxation: 4,
+            frictionEquationStiffness: 1e9,
+            frictionEquationRelaxation: 4,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <RecoilRoot>
+            <Suspense fallback={null}>
+              <Ball position={[0, 2, 0]} />
+              <Box position={[0, 2, 0]} setHudKey={setHudKey} hudKey={"face"} />
+              <Plane />
+              <Spotlight/>
+              {/* <Environment preset="forest"/> */}
+            </Suspense>
+          </RecoilRoot>
+        </Physics>
+      </Canvas>
     </div>
   );
 }
